8dc09d065ed12e70bebba78e91aa7e2e83be0499
MSUNDGR1 <50254400+MSUNDGR1@users.noreply.github.com>
2020-08-23 19:58:21 -0400

    Writing Cuda kernel for step function

--- CUDA_GOL/GOL_runner.cu
+++ CUDA_GOL/GOL_runner.cu
@@ -4,7 +4,20 @@
 #define threadHeight 16
 
 
+__global__ void stepper(bool* board, bool* newBoard, int* width, int* height) {
+	int horizIndex = blockIdx.x * blockDim.x + threadIdx.x;
+	int vertIndex = blockIdx.y * blockDim.y + threadIdx.y;
+	if (horizIndex < *width && vertIndex < *height) {
+		int neighborCount = 0;
+		int checkInd = horizIndex + 1;
+		checkInd %= *width;
+		checkInd = vertIndex * *width + checkInd;
+		if (board[checkInd]) neighborCount++;
+		checkInd = horizIndex - 1;
+		checkInd += *width; checkInd %= *width;
 
+	}
+}
 
 GOL::GOL(int width, int height, bool* data) {
 	this->width = width;
